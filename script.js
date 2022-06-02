c30bed3b95fd4c3d531ede2863f77ee9fc3c2065
Renan Fernandes <renan.fernandes@betrybe.com>
2022-06-02 15:01:33 -0300

    mentoria quinta-feira

--- script.js
+++ script.js
@@ -19,27 +19,60 @@ let turno = true;
 function jogar(event) {
   const clicado = event.target;
   let jogador; // X ou O
+  let outroJogador;
   
   if(turno) { // 'X'       turno === true
     jogador = 'X';
+    outroJogador = 'O';
   } else { // 'O'
     jogador = 'O';
+    outroJogador = 'X'
   }
 
-  player.innerText = 'Jogador da vez ' + jogador;
-  clicado.innerText = jogador;
-  turno = !turno;
+  // a celula esta em branco
+  if (clicado.innerText === ''){
+    player.innerText = 'Jogador da vez ' + outroJogador;
+    clicado.innerText = jogador;
+    turno = !turno;
+    checarGanhou(jogador);
+  }
+}
+
+// retornar true se o jogador ganhou naquela combinacao
+// retornar false caso contrario.
+function ganhou(jogador, combinacao) {
+  let pontuacao = 0;
+  for (let posicaoIndice in combinacao) {
+    // se eu tenho o indice
+    // acesso o valor atraves do array[indice]
+      const indiceCelula = combinacao[posicaoIndice];
+      const celula = document.getElementById(indiceCelula);
+      // se o jogador eh igual ao conteudo da celula
+      if (celula.innerText === jogador) {
+        pontuacao += 1;
+      }
+  }
+
+  if (pontuacao === 3) {
+    return true;
+  } else {
+    // quando seu codigo encontrar return
+    // ele interrompe a execucao da funcao
+    return false;
+  }
 }
 
 function checarGanhou(jogador) {
   // for of itera sobre os valores
   // for in itera sobre os indices
   for (let combinacao of combinacoes) {
-    console.log(combinacao);
+    // com uma combinacao vou verificar se jogador
+    // ganhou naquela combinacao
+    if (ganhou(jogador, combinacao) === true) {
+      player.innerText = jogador + ' venceu!!!';
+    }
   }
 }
-
-checarGanhou('X');
 
 // gere 9 divs e as adicione na div grid
 function gerarDivs() {
@@ -54,4 +87,12 @@ function gerarDivs() {
 }
 
 gerarDivs();
+
+const reset = document.getElementById('reset')
+reset.addEventListener('click', function(event) {
+  const celulas = document.querySelectorAll('.celula');
+  for (const celula of celulas) {
+    celula.innerText = '';
+  }
+});
 
